5b0336db4c2b9ecb10fbc19e07874593ef89cd72
Ilya Rodionov <ris58h@gmail.com>
2022-11-21 14:10:58 +0400

    First try

---
+++ build.sh
@@ -0,0 +1,12 @@
+clang++ -o validatekeymaps -std=c++17 platform/**/*.cc platform/**/*.cpp \
+  -isystem platform/external/fmtlib/include \
+  -isystem platform/frameworks/native/include \
+  -isystem platform/frameworks/native/libs/gui/include \
+  -isystem platform/frameworks/native/libs/math/include \
+  -isystem platform/frameworks/native/libs/ui/include \
+  -isystem platform/libnativehelper/include_jni \
+  -isystem platform/system/core/libcutils/include \
+  -isystem platform/system/core/libsystem/include \
+  -isystem platform/system/core/libutils/include \
+  -isystem platform/system/libbase/include \
+  -isystem platform/system/logging/liblog/include
